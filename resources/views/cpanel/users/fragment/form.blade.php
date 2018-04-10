38b75d830d7e8a3778bd5dc2f8e022b4ccd710c1
Abraham Moguel <abrahamamc@hotmail.com>
2018-04-10 18:14:43 -0500

    Tamaños del form actualizados

--- resources/views/cpanel/users/fragment/form.blade.php
+++ resources/views/cpanel/users/fragment/form.blade.php
@@ -1,35 +1,35 @@
 <div class="row">
-    <div class="col-6">
+    <div class="col-md-6">
         <div class="form-group">
             {!! Form::label('name','Nombre') !!}
             {!! Form::text('name', null,['class'=>'form-control']) !!}
         </div>
     </div>
-    <div class="col-6">
+    <div class="col-md-6">
         <div class="form-group">
             {!! Form::label('apellidos','Apellido') !!}
             {!! Form::text('apellidos', null,['class'=>'form-control']) !!}
         </div>
     </div>
-    <div class="col-6">
+    <div class="col-md-6">
         <div class="form-group">
             {!! Form::label('direccion','Direccion') !!}
             {!! Form::text('direccion', null,['class'=>'form-control']) !!}
         </div>
     </div>
-    <div class="col-6">
+    <div class="col-md-6">
         <div class="form-group">
             {!! Form::label('telefono','Telefono') !!}
             {!! Form::number('telefono', null,['class'=>'form-control']) !!}
         </div>
     </div>
-    <div class="col-6">
+    <div class="col-md-6">
         <div class="form-group">
             {!! Form::label('email','Correo') !!}
             {!! Form::email('email', null,['class'=>'form-control']) !!}
         </div>
     </div>
-    <div class="col-6">
+    <div class="col-md-6">
         <div class="form-group">
             {!! Form::label('type','Tipo') !!}
             {!! Form::select('type',['Administrador'=>'Administrador',
@@ -39,13 +39,13 @@
 
         </div>
     </div>
-    <div class="col-6">
+    <div class="col-md-6">
         <div class="form-group">
             {!! Form::label('password','Contraseña') !!}
             {!! Form::password('password',['class'=>'form-control']) !!}
         </div>
     </div>
-    <div class="col-6">
+    <div class="col-md-6">
         <div class="form-group">
             {!! Form::label('password','Confirmar Contraseña') !!}
             {!! Form::password('password_confirmation',['class'=>'form-control']) !!}
--- resources/views/cpanel/users/fragment/form2.blade.php
+++ resources/views/cpanel/users/fragment/form2.blade.php
@@ -1,35 +1,35 @@
 <div class="row">
-    <div class="col-6">
+    <div class="col-md-6">
         <div class="form-group">
             {!! Form::label('name','Nombre') !!}
             {!! Form::text('name', null,['class'=>'form-control']) !!}
         </div>
     </div>
-    <div class="col-6">
+    <div class="col-md-6">
         <div class="form-group">
             {!! Form::label('apellidos','Apellido') !!}
             {!! Form::text('apellidos', null,['class'=>'form-control']) !!}
         </div>
     </div>
-    <div class="col-6">
+    <div class="col-md-6">
         <div class="form-group">
             {!! Form::label('direccion','Direccion') !!}
             {!! Form::text('direccion', null,['class'=>'form-control']) !!}
         </div>
     </div>
-    <div class="col-6">
+    <div class="col-md-6">
         <div class="form-group">
             {!! Form::label('telefono','Telefono') !!}
             {!! Form::number('telefono', null,['class'=>'form-control']) !!}
         </div>
     </div>
-    <div class="col-6">
+    <div class="col-md-6">
         <div class="form-group">
             {!! Form::label('email','Correo') !!}
             {!! Form::email('email', null,['class'=>'form-control']) !!}
         </div>
     </div>
-    <div class="col-6">
+    <div class="col-md-6">
         <div class="form-group">
             {!! Form::label('type','Tipo') !!}
             {!! Form::select('type',['Administrador'=>'Administrador',
