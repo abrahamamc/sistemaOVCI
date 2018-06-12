6d32497ef93cc2360ba2ad6bc6308a3ebcbdb6bf
Abraham Moguel <abrahamamc@hotmail.com>
2018-06-12 01:45:06 -0500

    diseño seccion de productos

--- resources/views/productos/index.blade.php
+++ resources/views/productos/index.blade.php
@@ -4,4 +4,102 @@
 
 <h1> Productos</h1>
 
+<div class="container">
+
+    <div class="row">
+
+        <div class="col-md-3">
+            <h4>Menú productos</h4>
+        </div>
+
+        <div class="col-md-9">
+            <h4>productos</h4>
+
+        <div class="card-columns">
+
+                <div class="card">
+                        <img class="card-img-top" src="img/img-ejemplo.jpg" alt="Card image cap">
+                        <div class="card-body">
+                          <h5 class="card-title">Producto 1</h5>
+                          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+
+                        <div class="card-footer">
+                            <small class="">Precio: $100</small>
+                        </div>
+                      </div>
+
+                      <div class="card">
+                            <img class="card-img-top" src="img/img-ejemplo.jpg" alt="Card image cap">
+                            <div class="card-body">
+                              <h5 class="card-title">Producto 2</h5>
+                              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            </div>
+
+                            <div class="card-footer">
+                                    <small class="">Precio: $100</small>
+                                </div>
+                          </div>
+
+                          <div class="card">
+                                <img class="card-img-top" src="img/img-ejemplo.jpg" alt="Card image cap">
+                                <div class="card-body">
+                                  <h5 class="card-title">Producto 3</h5>
+                                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                </div>
+
+                                <div class="card-footer">
+                                        <small class="">Precio: $100</small>
+                                    </div>
+                            </div>
+
+        </div>
+
+        <div class="card-columns">
+
+                <div class="card">
+                        <img class="card-img-top" src="img/img-ejemplo.jpg" alt="Card image cap">
+                        <div class="card-body">
+                          <h5 class="card-title">Producto 1</h5>
+                          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+
+                        <div class="card-footer">
+                                <small class="">Precio: $100</small>
+                            </div>
+                      </div>
+
+                      <div class="card">
+                            <img class="card-img-top" src="img/img-ejemplo.jpg" alt="Card image cap">
+                            <div class="card-body">
+                              <h5 class="card-title">Producto 2</h5>
+                              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            </div>
+
+                            <div class="card-footer">
+                                    <small class="">Precio: $100</small>
+                                </div>
+                          </div>
+
+                          <div class="card">
+                                <img class="card-img-top" src="img/img-ejemplo.jpg" alt="Card image cap">
+                                <div class="card-body">
+                                  <h5 class="card-title">Producto 3</h5>
+                                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                </div>
+
+                                <div class="card-footer">
+                                        <small class="">Precio: $100</small>
+                                    </div>
+                            </div>
+
+        </div>
+
+            
+        </div>
+
+    </div>
+
+</div>
+
 @endsection
